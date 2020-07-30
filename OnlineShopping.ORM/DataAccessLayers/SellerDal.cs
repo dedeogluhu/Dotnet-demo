226897c1849a3517f622455950b92c822320d86a
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-07-30 17:30:25 +0300

    Finished SellerDal Class

--- OnlineShopping.ORM/DataAccessLayers/SellerDal.cs
+++ OnlineShopping.ORM/DataAccessLayers/SellerDal.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using OnlineShopping.Entity;
+using OnlineShopping.ORM.Tools;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +12,68 @@ namespace OnlineShopping.ORM.DataAccessLayers
 {
     public static class SellerDal
     {
+        public static List<Seller> Select()
+        {
+            SqlCommand sqlCommand = new SqlCommand("prc_Sellers_Select", Utilities.Connection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            List<Seller> sellers = new List<Seller>();
+
+            Utilities.OpenConnection(sqlCommand);
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            Utilities.CloseConnection(sqlCommand);
+
+            while (sqlDataReader.Read())
+            {
+                Seller seller = new Seller
+                {
+                    Id = Convert.ToInt32(sqlDataReader["Id"]),
+                    CompanyName = sqlDataReader["CompanyName"].ToString(),
+                    SellerId = sqlDataReader["SellerId"].ToString(),
+                    Password = sqlDataReader["Password"].ToString(),
+                    Cash = Convert.ToDecimal(sqlDataReader["Cash"])
+                };
+
+                sellers.Add(seller);
+            }
+
+            return sellers;
+        }
+
+        public static bool Insert(Seller seller)
+        {
+            SqlCommand sqlCommand = new SqlCommand("prc_Sellers_Insert", Utilities.Connection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@SellerId", seller.SellerId);
+            sqlCommand.Parameters.AddWithValue("@Password", seller.Password);
+            sqlCommand.Parameters.AddWithValue("@CompanyName", seller.CompanyName);
+            sqlCommand.Parameters.AddWithValue("@Cash", seller.Cash);
+
+            return Utilities.ExecuteNonQuery(sqlCommand);
+        }
+
+        public static bool Update(Seller seller)
+        {
+            SqlCommand sqlCommand = new SqlCommand("prc_Sellers_Update", Utilities.Connection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@Id", seller.Id);
+            sqlCommand.Parameters.AddWithValue("@SellerId", seller.SellerId);
+            sqlCommand.Parameters.AddWithValue("@Password", seller.Password);
+            sqlCommand.Parameters.AddWithValue("@CompanyName", seller.CompanyName);
+            sqlCommand.Parameters.AddWithValue("@Cash", seller.Cash);
+
+            return Utilities.ExecuteNonQuery(sqlCommand);
+        }
+
+        public static bool Delete(int id)
+        {
+            SqlCommand sqlCommand = new SqlCommand("prc_Sellers_Delete", Utilities.Connection);
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.Parameters.AddWithValue("@Id", id);
+
+            return Utilities.ExecuteNonQuery(sqlCommand);
+        }
     }
 }
