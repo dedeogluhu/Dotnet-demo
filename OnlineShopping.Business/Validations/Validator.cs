e201e4a3d2bb7fc349b536e90f8d3601d24f27b7
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-08-03 15:07:23 +0300

    Added Validator

--- OnlineShopping.Business/Validations/Validator.cs
+++ OnlineShopping.Business/Validations/Validator.cs
@@ -1,4 +1,7 @@
-﻿using OnlineShopping.ORM.Interfaces;
+﻿using OnlineShopping.Business.Interfaces;
+using OnlineShopping.Entity;
+using OnlineShopping.ORM.DataAccessLayers;
+using OnlineShopping.ORM.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +10,36 @@ using System.Threading.Tasks;
 
 namespace OnlineShopping.Business.Validations
 {
-    public class Validator<T> where T : IEntityDal
+    public class Validator : IValidator
     {
-        public static bool Validate(string id, string password)
+        public static bool ValidateCustomer(string id, string password)
         {
-            return false;
+            bool foundEntity = false;
+
+            foreach (Customer customer in CustomerDal.Select())
+            {
+                if (customer.CustomerId == id && customer.Password == password)
+                {
+                    foundEntity = true;
+                }
+            }
+
+            return foundEntity;
+        }
+
+        public static bool ValidateSeller(string id, string password)
+        {
+            bool foundEntity = false;
+
+            foreach (Seller seller in SellerDal.Select())
+            {
+                if (seller.SellerId == id && seller.Password == password)
+                {
+                    foundEntity = true;
+                }
+            }
+
+            return foundEntity;
         }
     }
 }
