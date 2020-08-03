57dd1e8bcb45ede35773cc78c08053e3c2599389
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-08-03 13:23:58 +0300

    Added Customer Validation Forms

---
+++ OnlineShopping.WinForms.UI/ValidationForms/CustomerLoginForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace OnlineShopping.WinForms.UI.ValidationForms
+{
+    partial class CustomerLoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustomerLoginId = new System.Windows.Forms.Label();
+            this.tbxCustomerLoginId = new System.Windows.Forms.TextBox();
+            this.lblCustomerLoginPassword = new System.Windows.Forms.Label();
+            this.tbxCustomerLoginPassword = new System.Windows.Forms.TextBox();
+            this.btnCustomerLoginToSignup = new System.Windows.Forms.Button();
+            this.lblCustomerLoginToSignup = new System.Windows.Forms.Label();
+            this.btnCustomerLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCustomerLoginId
+            // 
+            this.lblCustomerLoginId.AutoSize = true;
+            this.lblCustomerLoginId.Location = new System.Drawing.Point(52, 125);
+            this.lblCustomerLoginId.Name = "lblCustomerLoginId";
+            this.lblCustomerLoginId.Size = new System.Drawing.Size(25, 17);
+            this.lblCustomerLoginId.TabIndex = 0;
+            this.lblCustomerLoginId.Text = "ID:";
+            // 
+            // tbxCustomerLoginId
+            // 
+            this.tbxCustomerLoginId.Location = new System.Drawing.Point(55, 172);
+            this.tbxCustomerLoginId.Name = "tbxCustomerLoginId";
+            this.tbxCustomerLoginId.Size = new System.Drawing.Size(264, 22);
+            this.tbxCustomerLoginId.TabIndex = 1;
+            // 
+            // lblCustomerLoginPassword
+            // 
+            this.lblCustomerLoginPassword.AutoSize = true;
+            this.lblCustomerLoginPassword.Location = new System.Drawing.Point(52, 278);
+            this.lblCustomerLoginPassword.Name = "lblCustomerLoginPassword";
+            this.lblCustomerLoginPassword.Size = new System.Drawing.Size(73, 17);
+            this.lblCustomerLoginPassword.TabIndex = 2;
+            this.lblCustomerLoginPassword.Text = "Password:";
+            // 
+            // tbxCustomerLoginPassword
+            // 
+            this.tbxCustomerLoginPassword.Location = new System.Drawing.Point(55, 330);
+            this.tbxCustomerLoginPassword.Name = "tbxCustomerLoginPassword";
+            this.tbxCustomerLoginPassword.Size = new System.Drawing.Size(264, 22);
+            this.tbxCustomerLoginPassword.TabIndex = 3;
+            // 
+            // btnCustomerLoginToSignup
+            // 
+            this.btnCustomerLoginToSignup.Location = new System.Drawing.Point(265, 13);
+            this.btnCustomerLoginToSignup.Name = "btnCustomerLoginToSignup";
+            this.btnCustomerLoginToSignup.Size = new System.Drawing.Size(105, 27);
+            this.btnCustomerLoginToSignup.TabIndex = 4;
+            this.btnCustomerLoginToSignup.Text = "Signup";
+            this.btnCustomerLoginToSignup.UseVisualStyleBackColor = true;
+            // 
+            // lblCustomerLoginToSignup
+            // 
+            this.lblCustomerLoginToSignup.AutoSize = true;
+            this.lblCustomerLoginToSignup.Location = new System.Drawing.Point(13, 13);
+            this.lblCustomerLoginToSignup.Name = "lblCustomerLoginToSignup";
+            this.lblCustomerLoginToSignup.Size = new System.Drawing.Size(185, 17);
+            this.lblCustomerLoginToSignup.TabIndex = 5;
+            this.lblCustomerLoginToSignup.Text = "You don\'t have an account?";
+            // 
+            // btnCustomerLogin
+            // 
+            this.btnCustomerLogin.Location = new System.Drawing.Point(124, 413);
+            this.btnCustomerLogin.Name = "btnCustomerLogin";
+            this.btnCustomerLogin.Size = new System.Drawing.Size(123, 49);
+            this.btnCustomerLogin.TabIndex = 6;
+            this.btnCustomerLogin.Text = "Login";
+            this.btnCustomerLogin.UseVisualStyleBackColor = true;
+            // 
+            // CustomerLoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 553);
+            this.Controls.Add(this.btnCustomerLogin);
+            this.Controls.Add(this.lblCustomerLoginToSignup);
+            this.Controls.Add(this.btnCustomerLoginToSignup);
+            this.Controls.Add(this.tbxCustomerLoginPassword);
+            this.Controls.Add(this.lblCustomerLoginPassword);
+            this.Controls.Add(this.tbxCustomerLoginId);
+            this.Controls.Add(this.lblCustomerLoginId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "CustomerLoginForm";
+            this.Text = "CustomerLoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCustomerLoginId;
+        private System.Windows.Forms.TextBox tbxCustomerLoginId;
+        private System.Windows.Forms.Label lblCustomerLoginPassword;
+        private System.Windows.Forms.TextBox tbxCustomerLoginPassword;
+        private System.Windows.Forms.Button btnCustomerLoginToSignup;
+        private System.Windows.Forms.Label lblCustomerLoginToSignup;
+        private System.Windows.Forms.Button btnCustomerLogin;
+    }
+}
---
+++ OnlineShopping.WinForms.UI/ValidationForms/CustomerSignupForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace OnlineShopping.WinForms.UI.ValidationForms
+{
+    partial class CustomerSignupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustomerSignupToLogin = new System.Windows.Forms.Label();
+            this.btnCustomerSignupToLogin = new System.Windows.Forms.Button();
+            this.btnCustomerSignup = new System.Windows.Forms.Button();
+            this.lblCustomerSignupId = new System.Windows.Forms.Label();
+            this.lblCustomerSignupPassword = new System.Windows.Forms.Label();
+            this.lblCustomerSignupFirstName = new System.Windows.Forms.Label();
+            this.lblCustomerSignupLastName = new System.Windows.Forms.Label();
+            this.tbxCustomerSignupId = new System.Windows.Forms.TextBox();
+            this.tbxCustomerSignupPassword = new System.Windows.Forms.TextBox();
+            this.tbxCustomerSignupFirstName = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblCustomerSignupToLogin
+            // 
+            this.lblCustomerSignupToLogin.AutoSize = true;
+            this.lblCustomerSignupToLogin.Location = new System.Drawing.Point(12, 13);
+            this.lblCustomerSignupToLogin.Name = "lblCustomerSignupToLogin";
+            this.lblCustomerSignupToLogin.Size = new System.Drawing.Size(201, 17);
+            this.lblCustomerSignupToLogin.TabIndex = 0;
+            this.lblCustomerSignupToLogin.Text = "You already have an account?";
+            // 
+            // btnCustomerSignupToLogin
+            // 
+            this.btnCustomerSignupToLogin.Location = new System.Drawing.Point(258, 5);
+            this.btnCustomerSignupToLogin.Name = "btnCustomerSignupToLogin";
+            this.btnCustomerSignupToLogin.Size = new System.Drawing.Size(112, 32);
+            this.btnCustomerSignupToLogin.TabIndex = 1;
+            this.btnCustomerSignupToLogin.Text = "Login";
+            this.btnCustomerSignupToLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCustomerSignup
+            // 
+            this.btnCustomerSignup.Location = new System.Drawing.Point(110, 459);
+            this.btnCustomerSignup.Name = "btnCustomerSignup";
+            this.btnCustomerSignup.Size = new System.Drawing.Size(140, 46);
+            this.btnCustomerSignup.TabIndex = 2;
+            this.btnCustomerSignup.Text = "Signup";
+            this.btnCustomerSignup.UseVisualStyleBackColor = true;
+            // 
+            // lblCustomerSignupId
+            // 
+            this.lblCustomerSignupId.AutoSize = true;
+            this.lblCustomerSignupId.Location = new System.Drawing.Point(33, 102);
+            this.lblCustomerSignupId.Name = "lblCustomerSignupId";
+            this.lblCustomerSignupId.Size = new System.Drawing.Size(25, 17);
+            this.lblCustomerSignupId.TabIndex = 3;
+            this.lblCustomerSignupId.Text = "ID:";
+            // 
+            // lblCustomerSignupPassword
+            // 
+            this.lblCustomerSignupPassword.AutoSize = true;
+            this.lblCustomerSignupPassword.Location = new System.Drawing.Point(33, 189);
+            this.lblCustomerSignupPassword.Name = "lblCustomerSignupPassword";
+            this.lblCustomerSignupPassword.Size = new System.Drawing.Size(73, 17);
+            this.lblCustomerSignupPassword.TabIndex = 4;
+            this.lblCustomerSignupPassword.Text = "Password:";
+            // 
+            // lblCustomerSignupFirstName
+            // 
+            this.lblCustomerSignupFirstName.AutoSize = true;
+            this.lblCustomerSignupFirstName.Location = new System.Drawing.Point(33, 276);
+            this.lblCustomerSignupFirstName.Name = "lblCustomerSignupFirstName";
+            this.lblCustomerSignupFirstName.Size = new System.Drawing.Size(76, 17);
+            this.lblCustomerSignupFirstName.TabIndex = 5;
+            this.lblCustomerSignupFirstName.Text = "FirstName:";
+            // 
+            // lblCustomerSignupLastName
+            // 
+            this.lblCustomerSignupLastName.AutoSize = true;
+            this.lblCustomerSignupLastName.Location = new System.Drawing.Point(33, 363);
+            this.lblCustomerSignupLastName.Name = "lblCustomerSignupLastName";
+            this.lblCustomerSignupLastName.Size = new System.Drawing.Size(76, 17);
+            this.lblCustomerSignupLastName.TabIndex = 6;
+            this.lblCustomerSignupLastName.Text = "LastName:";
+            // 
+            // tbxCustomerSignupId
+            // 
+            this.tbxCustomerSignupId.Location = new System.Drawing.Point(36, 126);
+            this.tbxCustomerSignupId.Name = "tbxCustomerSignupId";
+            this.tbxCustomerSignupId.Size = new System.Drawing.Size(311, 22);
+            this.tbxCustomerSignupId.TabIndex = 7;
+            // 
+            // tbxCustomerSignupPassword
+            // 
+            this.tbxCustomerSignupPassword.Location = new System.Drawing.Point(36, 217);
+            this.tbxCustomerSignupPassword.Name = "tbxCustomerSignupPassword";
+            this.tbxCustomerSignupPassword.Size = new System.Drawing.Size(311, 22);
+            this.tbxCustomerSignupPassword.TabIndex = 8;
+            // 
+            // tbxCustomerSignupFirstName
+            // 
+            this.tbxCustomerSignupFirstName.Location = new System.Drawing.Point(36, 308);
+            this.tbxCustomerSignupFirstName.Name = "tbxCustomerSignupFirstName";
+            this.tbxCustomerSignupFirstName.Size = new System.Drawing.Size(311, 22);
+            this.tbxCustomerSignupFirstName.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(36, 399);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(311, 22);
+            this.textBox4.TabIndex = 10;
+            // 
+            // CustomerSignupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 553);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tbxCustomerSignupFirstName);
+            this.Controls.Add(this.tbxCustomerSignupPassword);
+            this.Controls.Add(this.tbxCustomerSignupId);
+            this.Controls.Add(this.lblCustomerSignupLastName);
+            this.Controls.Add(this.lblCustomerSignupFirstName);
+            this.Controls.Add(this.lblCustomerSignupPassword);
+            this.Controls.Add(this.lblCustomerSignupId);
+            this.Controls.Add(this.btnCustomerSignup);
+            this.Controls.Add(this.btnCustomerSignupToLogin);
+            this.Controls.Add(this.lblCustomerSignupToLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "CustomerSignupForm";
+            this.Text = "CustomerSignupForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCustomerSignupToLogin;
+        private System.Windows.Forms.Button btnCustomerSignupToLogin;
+        private System.Windows.Forms.Button btnCustomerSignup;
+        private System.Windows.Forms.Label lblCustomerSignupId;
+        private System.Windows.Forms.Label lblCustomerSignupPassword;
+        private System.Windows.Forms.Label lblCustomerSignupFirstName;
+        private System.Windows.Forms.Label lblCustomerSignupLastName;
+        private System.Windows.Forms.TextBox tbxCustomerSignupId;
+        private System.Windows.Forms.TextBox tbxCustomerSignupPassword;
+        private System.Windows.Forms.TextBox tbxCustomerSignupFirstName;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
